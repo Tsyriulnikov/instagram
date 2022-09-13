da19091ed6f744584229535b99aae23e0c42bee2
Tsyriulnikov <max_djvu@mail.ru>
2022-09-13 20:09:11 +0300

    test 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+
+interface IUser {
+    age: number
+    name: string
+}
+
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'inst-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'instagram';
+    title = 'instagram';
+    user: IUser = {
+        age: 50,
+        name: 'Chuka'
+    }
+    isAppLoading = true
+    changeTitleHandler () {
+        this.title='Title Changed'
+    };
+
+    constructor() {
+        setTimeout(() => {
+            this.isAppLoading = false
+        },3000)
+    }
+
+
 }
