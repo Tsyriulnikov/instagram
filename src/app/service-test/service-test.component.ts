56da371725d904c394aedc1baf89d236aa7340e5
Tsyriulnikov <max_djvu@mail.ru>
2022-09-15 19:58:13 +0300

    test 5

--- src/app/service-test/service-test.component.ts
+++ src/app/service-test/service-test.component.ts
@@ -1,16 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ValueService} from "../services/value.service";
 
 @Component({
-  selector: 'inst-service-test',
-  templateUrl: './service-test.component.html',
-  styleUrls: ['./service-test.component.css']
+    selector: 'inst-service-test',
+    templateUrl: './service-test.component.html',
+    styleUrls: ['./service-test.component.css'],
+// providers:[ValueService]
 })
 export class ServiceTestComponent implements OnInit {
 
-  constructor(private valueService:ValueService) { }
+    constructor(private valueService: ValueService) {
+    }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+    }
 
 }
