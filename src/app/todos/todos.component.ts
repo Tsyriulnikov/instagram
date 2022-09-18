8b72ef6278c9eea1bf88f4d281128ee070d20a29
Tsyriulnikov <max_djvu@mail.ru>
2022-09-18 15:49:48 +0300

    http post

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import {Observable, tap} from "rxjs";
 
 interface Todo {
     addedDate: string
@@ -8,10 +9,24 @@ interface Todo {
     title: string
 }
 
-interface CreateTodoResponse {
-    data: {
-        item: Todo
-    }
+// interface CreateTodoResponse {
+//     data: {
+//         item: Todo
+//     }
+//     messages: string[]
+//     fieldsErrors: string[]
+//     resultCode: number
+// }
+//
+// interface DeleteTodoResponse {
+//     data: {}
+//     messages: string[]
+//     fieldsErrors: string[]
+//     resultCode: number
+// }
+
+interface BaseResponse<T = {}>{
+    data:T
     messages: string[]
     fieldsErrors: string[]
     resultCode: number
@@ -39,23 +54,38 @@ export class TodosComponent implements OnInit {
     }
 
     getTodos() {
-        this.http.get<Todo[]>('https://social-network.samuraijs.com/api/1.1/todo-lists', this.httpOptions)
+        this.http.get<Todo[]>(`https://social-network.samuraijs.com/api/1.1/todo-lists`, this.httpOptions)
             .subscribe((res) => {
                 this.todos = res
             })
+
     }
+
+    // getTodos(): Observable<Todo[]> {
+    //     return this.http.get<Todo[]>(`https://social-network.samuraijs.com/api/1.1/todo-lists`, this.httpOptions)
+    //         .pipe(
+    //             tap(_ => this.todos),
+    //             // catchError(this.handleError<Todo[]>('getHeroes', []))
+    //         );
+    // }
 
     createTodo() {
         const randomNamber = Math.floor(Math.random() * 100)
         const title = 'Angular' + randomNamber
-        this.http.post<CreateTodoResponse>('https://social-network.samuraijs.com/api/1.1/todo-lists', {title}, this.httpOptions)
+        this.http.post<BaseResponse<{item:Todo}>>(`https://social-network.samuraijs.com/api/1.1/todo-lists`, {title}, this.httpOptions)
             .subscribe(res => {
-            const newTodo=res.data.item
+                const newTodo = res.data.item
                 this.todos.unshift(newTodo)
             })
     }
 
     deleteTodo() {
+        const todoId = this.todos[0].id
+        this.http
+            .delete<BaseResponse>(`https://social-network.samuraijs.com/api/1.1/todo-lists/${todoId}`, this.httpOptions)
+            .subscribe(() => {
+                this.todos = this.todos.filter(tl => tl.id !== todoId)
+            })
 
     }
 }
