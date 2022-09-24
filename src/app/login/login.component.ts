f860656a70d5e44a87075f780e7e74a68e5fd1e7
Tsyriulnikov <max_djvu@mail.ru>
2022-09-24 15:58:48 +0300

    formcontrol2

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import {FormControl} from "@angular/forms";
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
-  selector: 'inst-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+    selector: 'inst-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
-  email = new FormControl('')
-  constructor() { }
+    // email = new FormControl('')
+    loginForm = new FormGroup({
+        email: new FormControl('', Validators.required),
+        password: new FormControl('')
+    })
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
 
+    ngOnInit(): void {
+    }
+
+    get email() {
+        return this.loginForm.get('email')
+    }
+
+    onSubmit() {
+        alert(JSON.stringify(this.loginForm.value))
+    }
 }
