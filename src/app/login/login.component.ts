fb59b77a339b6db95de3b81ad6058b2ee488e138
Tsyriulnikov <max_djvu@mail.ru>
2022-09-24 18:50:05 +0300

    formcontrol3

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,8 +10,9 @@ export class LoginComponent implements OnInit {
 
     // email = new FormControl('')
     loginForm = new FormGroup({
-        email: new FormControl('', Validators.required),
-        password: new FormControl('')
+        email: new FormControl('', [Validators.required,
+            Validators.pattern('[A-Za-z0-9._%-]+@[A-Za-z0-9._%-]+\\.[a-z]{2,5}$')]),
+        password: new FormControl('', Validators.pattern('[a-zA-Z]*'))
     })
 
     constructor() {
