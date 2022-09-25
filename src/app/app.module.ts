02fd0dfc5b02e6420df3f2d615ebdc02aa69365f
Tsyriulnikov <max_djvu@mail.ru>
2022-09-25 18:13:59 +0300

    routing1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,11 +12,25 @@ import { TodosComponent } from './todos/todos.component';
 import {HttpClientModule} from "@angular/common/http";
 import { ObservableTestComponent } from './observable-test/observable-test.component';
 import { LoginComponent } from './login/login.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './navigation/home/home.component';
+import { UsersComponent } from './navigation/users/users.component';
+import { ProfileComponent } from './navigation/profile/profile.component';
+import { PageNotFoundComponent } from './navigation/page-not-found/page-not-found.component';
+import {RouterModule} from "@angular/router";
 
 
 @NgModule({
-  declarations: [AppComponent, ParentComponent, ChildComponent, Issue1Component, ServiceTestComponent, TodosComponent, ObservableTestComponent,LoginComponent    ],
-    imports: [BrowserModule, AppRoutingModule, FormsModule,HttpClientModule,ReactiveFormsModule],
+  declarations: [AppComponent, ParentComponent, ChildComponent, Issue1Component, ServiceTestComponent, TodosComponent, ObservableTestComponent,LoginComponent, NavigationComponent, HomeComponent, UsersComponent, ProfileComponent, PageNotFoundComponent    ],
+    imports: [BrowserModule, AppRoutingModule, FormsModule,HttpClientModule,ReactiveFormsModule,
+      RouterModule.forRoot([
+        {path:'',component:HomeComponent},
+        {path:'login', component:LoginComponent},
+        {path:'profile', component:ProfileComponent},
+        {path:'todos', component:TodosComponent},
+        {path:'users', component:UsersComponent},
+      ])
+    ],
   providers: [],
   bootstrap: [AppComponent],
 })
